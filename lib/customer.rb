86b25990b9b376877babe76e21e5adc49f4c1599
Ahmad hamza <ahmad.hamza@crealytics.com>
2018-09-04 18:19:59 +0530

    Replace temp with query for total

--- lib/customer.rb
+++ lib/customer.rb
@@ -23,12 +23,21 @@ class Customer
 
      # show figures for this rental
      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
-     total_amount += element.charge
    end
 
    # add footer lines
-   result += "Amount owned is #{total_amount}\n"
+   result += "Amount owned is #{total_charge}\n"
    result += "You earned #{frequent_renter_points} frequent renter points"
+   result
+ end
+
+ private
+
+ def total_charge
+   result = 0
+   @rentals.each do |rental|
+     result += rental.charge
+   end
    result
  end
 end
