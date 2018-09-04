3deb4518f91253fa212a7c20288da5b44dd046f3
Ahmad hamza <ahmad.hamza@crealytics.com>
2018-09-04 18:23:01 +0530

    Replace temp with query and collection closure with inject

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,17 +17,13 @@ class Customer
    total_amount, frequent_renter_points = 0, 0
    result = "Rental Record for #{@name}\n"
    @rentals.each do |element|
-
-     # add frequent renter points
-     frequent_renter_points += element.frequent_renter_points
-
      # show figures for this rental
      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
    end
 
    # add footer lines
    result += "Amount owned is #{total_charge}\n"
-   result += "You earned #{frequent_renter_points} frequent renter points"
+   result += "You earned #{total_frequent_renter_points} frequent renter points"
    result
  end
 
@@ -35,5 +31,9 @@ class Customer
 
  def total_charge
    @rentals.inject(0) { |sum, rental| sum + rental.charge }
+ end
+
+ def total_frequent_renter_points
+   @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
  end
 end
