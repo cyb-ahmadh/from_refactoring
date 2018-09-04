661eccf2aaf077c2c9940253bf3e24047c98c561
Ahmad hamza <ahmadhamza19@gmail.com>
2018-09-05 00:09:48 +0530

    html statement added with specs

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,20 +2,20 @@ require 'byebug'
 require 'customer'
 
 describe Customer do
+  let(:movie) do
+    Movie.new(movie_name, price_code)
+  end
+
+  let(:rental) do
+    Rental.new(movie, days_rented)
+  end
+
+  let(:customer) do
+    name = 'Ahmad'
+    Customer.new(name)
+  end
+
   describe 'Statement' do
-    let(:movie) do
-      Movie.new(movie_name, price_code)
-    end
-
-    let(:rental) do
-      Rental.new(movie, days_rented)
-    end
-
-    let(:customer) do
-      name = 'Ahmad'
-      Customer.new(name)
-    end
-
     subject do
       customer.statement
     end
@@ -50,6 +50,25 @@ describe Customer do
 
       it 'should match the statement' do
         expect(subject).to eq("Rental Record for Ahmad\n\tMowgli\t3.0\nAmount owned is 3.0\nYou earned 1 frequent renter points")
+      end
+    end
+  end
+
+  describe 'Html Statement' do
+    subject do
+      customer.html_statement
+    end
+
+    before do
+      customer.add_rental(rental)
+    end
+
+    context 'new release' do
+      let(:movie_name) { 'Stree' }
+      let(:price_code) { 1 }
+      let(:days_rented) { 3 }
+      it "Should match the html" do
+        expect(subject).to eq("<h1>Rentals for <em>Ahmad</em></h1><p>\n\tStree: 9<br>\n<p>You owe <em>9</em><p>\nOn this rental you earned <em>2</em> frequent renter points<p>")
       end
     end
   end
