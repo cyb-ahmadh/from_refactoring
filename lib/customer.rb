dcfad5bc25426f60d864faff04895aed17711e13
Ahmad hamza <ahmad.hamza@crealytics.com>
2018-09-04 18:10:22 +0530

    extract method on frequent renter points

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,12 +19,7 @@ class Customer
    @rentals.each do |element|
 
      # add frequent renter points
-     frequent_renter_points += 1
-
-     # add bonus for a two day new release rental
-     if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-       frequent_renter_points += 1
-     end
+     frequent_renter_points += element.frequent_renter_points
 
      # show figures for this rental
      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
